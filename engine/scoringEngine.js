e3e4690b605ffeb350c79957c14c823c0dc11285
ThePMYomi <thepmyomi@gmail.com>
2026-03-12 08:19:46 +0100

    Update scoringEngine.js

--- engine/scoringEngine.js
+++ engine/scoringEngine.js
@@ -19,68 +19,6 @@
 // distance 4 = 0
 
 
-export function scoreRanking(correctOrder, userOrder){
-
-    let score = 0
-
-    correctOrder.forEach((option, correctIndex) => {
-
-        const userIndex = userOrder.indexOf(option)
-
-        if(userIndex === -1) return
-
-        const distance = Math.abs(correctIndex - userIndex)
-
-        const points = Math.max(0, 4 - distance)
-
-        score += points
-
-    })
-
-    return score
-
-}
-
-
-
-// =======================
-// BEST-3 QUESTION SCORING
-// =======================
-
-// correct answers example:
-// ["A","C","F"]
-
-// user answers example:
-// ["A","F","D"]
-
-// scoring:
-// each correct = 4 marks
-
-
-export function scoreBest3(correctAnswers, userAnswers){
-
-    let score = 0
-
-    userAnswers.forEach(answer => {
-
-        if(correctAnswers.includes(answer)){
-
-            score += 4
-
-        }
-
-    })
-
-    return score
-
-}
-
-
-
-// =======================
-// TOTAL EXAM SCORE
-// =======================
-
 export function calculateTotalScore(examQuestions, userAnswers){
 
     let totalScore = 0
@@ -90,20 +28,22 @@ export function calculateTotalScore(examQuestions, userAnswers){
 
         const answer = userAnswers[i]
 
-        if(!answer) return
-
         if(q.type === "ranking"){
-
-            totalScore += scoreRanking(q.answer, answer)
             maxScore += 20
-
         }
 
         if(q.type === "best3"){
-
-            totalScore += scoreBest3(q.answer, answer)
             maxScore += 12
+        }
 
+        if(!answer) return
+
+        if(q.type === "ranking"){
+            totalScore += scoreRanking(q.answer, answer)
+        }
+
+        if(q.type === "best3"){
+            totalScore += scoreBest3(q.answer, answer)
         }
 
     })
